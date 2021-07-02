cbe8613da3bdd016dfa3c50980f0cb837e74b13d
veronika <vesparragozam@gmail.com>
2021-07-02 00:00:02 -0500

    Agregando animaciones

---
+++ src/js/index.js
@@ -0,0 +1,54 @@
+const resultadoLunaNueva = function () {
+    setTimeout(function(){ //Nueva
+        document.getElementById("QuetamañoWapa").innerHTML = "Nueva";
+    }, 9300);
+  }
+  const resultadoLunasCreciente = function () {
+    setTimeout(function(){ //Creciente
+        document.getElementById("QuetamañoWapa").innerHTML = "Creciente";
+    }, 2700);
+  }
+  const resultadoLunas4_1 = function () {
+    setTimeout(function(){ //Cuarto creciente
+        document.getElementById("QuetamañoWapa").innerHTML = "Cuarto creciente";
+    }, 5000);
+  }
+  const resultadoLunasGibosa1 = function () {
+    setTimeout(function(){ //Creciente gibosa
+        document.getElementById("QuetamañoWapa").innerHTML = "Creciente gibosa";
+    }, 1000);
+  }
+  const resultadoLunaLlena = function () {
+    setTimeout(function(){ //Llena
+        document.getElementById("QuetamañoWapa").innerHTML = "Llena";
+    }, 19000);
+  }
+  const resultadoLunasGibosa2 = function () {
+    setTimeout(function(){ //Menguante gibosa
+        document.getElementById("QuetamañoWapa").innerHTML = "Menguante gibosa";
+    }, 17000);
+  }
+  const resultadoLunas4_2 = function () {
+    setTimeout(function(){ //Cuarto menguante
+        document.getElementById("QuetamañoWapa").innerHTML = "Cuarto menguante";
+    }, 15500);
+  }
+  const resultadoLunaMenguante = function () {
+    setTimeout(function(){ //Menguante
+        document.getElementById("QuetamañoWapa").innerHTML = "Menguante";
+    }, 12000);
+  }
+  const facesDeLaLuna = function () {
+    setTimeout(function(){ //Llena
+        document.getElementById("QuetamañoWapa").innerHTML = "Faces de la luna";
+    }, 19999);
+  }
+    resultadoLunaNueva();
+    resultadoLunasCreciente();
+    resultadoLunas4_1();
+    resultadoLunasGibosa1();
+    resultadoLunaLlena();
+    resultadoLunasGibosa2();
+    resultadoLunas4_2();
+    resultadoLunaMenguante();
+    facesDeLaLuna();
